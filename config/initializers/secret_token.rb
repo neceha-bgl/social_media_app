8699f7a68dcece0be568ebe537f07900c8d1395f
neceha bgl <neceha.bgl@gmail.com>
2013-05-31 16:23:53 +0200

    Create the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialMediaApp::Application.config.secret_token = 'c543132cdae3c36a8c0c01ae1a4376e2222765abfa7b1af296664044514fd25c170772daa84be695072b65672a0518e7c33464caf672dbf58ce9f7394041589b'
