1e3d8f253e6ea70e275138a3976ef467c0325979
neceha bgl <neceha.bgl@gmail.com>
2013-06-01 15:03:43 +0200

    add custom attributes for user model

---
+++ db/migrate/20130601125644_add_custom_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddCustomAttributesToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :user_name, :string
+    add_column :users, :birthday, :date
+  end
+end
